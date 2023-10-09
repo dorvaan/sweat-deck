ebbf8831849884be237967bf7a12c6d69f450f88
dorvaan <104641899+dorvaan@users.noreply.github.com>
2023-10-09 14:03:48 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,35 @@
 const deck = [
   {
     id: 1,
+    type: "Workout",
     name: "Cheat Meal",
-    difficulty: 1,
-    type: "For Time",
+    difficulty: 3,
+    style: "For Time",
     description:
       "1 Mile Run, 25 Thrusters, 25 Pull-ups, 25 Push-ups, 50 Burpees, 1 Mile Run",
   },
   {
     id: 2,
-    name: "MEGA Cheat Meal",
+    type: "Workout",
+    name: "Break a Sweat",
     difficulty: 1,
-    type: "For Time",
-    description:
-      "1 Mile Run, 25 Thrusters, 25 Pull-ups, 25 Push-ups, 50 Burpees, 1 Mile Run",
+    style: "15 Min AMRAP",
+    description: "10 Pullups, 10 Burpees, 10 Air Squats",
   },
   {
     id: 3,
-    name: "Workout Is Coming!!!",
-    difficulty: 1,
-    type: "For Time",
+    type: "Workout",
+    name: "Workout Is Coming",
+    difficulty: 2,
+    style: "For Time",
     description: "800M Run, 50 Burpess, 50 KB Swings, 50 Pull-ups, 800M Run",
   },
   {
     id: 4,
+    type: "Finisher",
     name: "Workout Is Coming 2!!!",
     difficulty: 1,
-    type: "For Time",
+    style: "For Time",
     description: "1600M Run, 50 Burpess, 50 KB Swings, 50 Pull-ups, 800M Run",
   },
 ];
